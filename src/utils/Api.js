d0090b5f8c999ad86a371063b7c6502792fffd23
Sarah <sarahhargreaves10@gmail.com>
2019-11-28 17:16:57 -0500

    heroku connection

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://gentle-ravine-25371.herokuapp.com/",
     responseType: "json",
     headers: {"Access-Control-Allow-Origin": "*"}
 });
